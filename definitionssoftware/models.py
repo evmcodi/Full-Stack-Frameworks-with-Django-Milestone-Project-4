5f7cc70fc84ae6b69044d13221cb0ba2a71c42e9
evmcodi <emaccuarta@protonmail.com>
2020-07-28 03:41:29 +0100

    feat: Add Django permission to access the Definitions software page so only logged in and paid users can access the app.

--- definitionssoftware/models.py
+++ definitionssoftware/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 
+
+
 class Term(models.Model):
     # The user that has created the term object.
     # user = models.ForeignKey('User', null=True, blank=True, on_delete=models.SET_NULL)
@@ -11,5 +13,12 @@ class Term(models.Model):
     # The definition that the user enters.
     definition = models.CharField(max_length=4000, null=True, blank=True)
 
+
     def __str__(self):
         return self.term
+
+    class Meta:
+        permissions = [
+            ("access_paid_definitions_app", "Paid user - can access definitions app."),
+        ]
+
--- definitionssoftware/views.py
+++ definitionssoftware/views.py
@@ -1,4 +1,4 @@
-
+from django.contrib.auth.models import User
 from django.shortcuts import (
     render, redirect, reverse, HttpResponse, get_object_or_404
 )
@@ -6,8 +6,18 @@ from django.contrib import messages
 
 from definitionssoftware.models import Term
 
+from django.contrib.auth.decorators import permission_required, login_required
 
+
+@login_required
+# @permission_required('definitionssoftware.access_paid_definitions_app', raise_exception=True)
+# @permission_required('definitionssoftware.access_paid_definitions_app')
 def view_definitionssoftware(request):
     """ A view that renders the definitions software template """
 
-    return render(request, 'definitionssoftware/definitionssoftware.html')
+    user = request.user
+
+    if user.has_perm('definitionssoftware.access_paid_definitions_app'):
+        return render(request, 'definitionssoftware/definitionssoftware.html')
+
+    return render(request, 'home/index.html')
