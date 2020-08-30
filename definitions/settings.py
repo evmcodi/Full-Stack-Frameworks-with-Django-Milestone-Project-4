bc29fd9f98a375df936fd5e3a43fc7323330b76c
evmcodi <emaccuarta@protonmail.com>
2020-08-30 05:52:39 +0100

    feat: Allow deployment of static files to AWS S3 bucket.

--- definitions/settings.py
+++ definitions/settings.py
@@ -48,8 +48,10 @@ INSTALLED_APPS = [
     'definitionssoftware',
     'payment',
     'blog',
+
     # Additional apps
     'widget_tweaks',
+    'storages',
 
 ]
 
@@ -172,8 +174,8 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = ''
-    AWS_S3_REGION_NAME = ''
+    AWS_STORAGE_BUCKET_NAME = 'definitions-web-app'
+    AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
