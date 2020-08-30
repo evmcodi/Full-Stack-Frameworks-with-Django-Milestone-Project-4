c1fb4fc769287e7ac9a9f6af849dc9cc542b7b61
evmcodi <emaccuarta@protonmail.com>
2020-08-30 07:16:47 +0100

    feat: Add email configuration for sending verification emails.

--- definitions/settings.py
+++ definitions/settings.py
@@ -200,7 +200,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 # Email functionality backend
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+    DEFAULT_FROM_EMAIL = 'definition-web-app@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
