f3410ac28deec38ca499f4c161c715c43f47e263
evmcodi <emaccuarta@protonmail.com>
2020-08-30 19:33:02 +0100

    feat: Add javascript buttons for collapse and expand of definitions and terms.

--- definitionssoftware/static/definitionssoftware/js/definitionssoftware.js
+++ definitionssoftware/static/definitionssoftware/js/definitionssoftware.js
@@ -1,62 +1,71 @@
+// Expand and collapse all term & definition elements.
 
-// This javascript file is based on this tutorial: 'How to Implement CRUD Using Ajax and Json'
+function defiExpand() {
+    $("details").attr("open","");
+}
+
+function defiCollapse() {
+    $("details").removeAttr("open");
+}
+
+
+// This javascript Ajax functionality is based on this tutorial: 'How to Implement CRUD Using Ajax and Json'
 // https://simpleisbetterthancomplex.com/tutorial/2016/11/15/how-to-implement-a-crud-using-ajax-and-json.html
 // Some code was changed to correspond to Definitions app.
 
 
 $(function () {
 
-  /* Functions */
+    /* Functions */
 
-  var loadForm = function () {
-    var btn = $(this);
-    $.ajax({
-      url: btn.attr("data-url"),
-      type: 'get',
-      dataType: 'json',
-      beforeSend: function () {
-        $("#modal-term .modal-content").html("");
-        $("#modal-term").modal("show");
-      },
-      success: function (data) {
-        $("#modal-term .modal-content").html(data.html_form);
-      }
-    });
-  };
+    var loadForm = function () {
+        var btn = $(this);
+        $.ajax({
+            url: btn.attr("data-url"),
+            type: 'get',
+            dataType: 'json',
+            beforeSend: function () {
+                $("#modal-term .modal-content").html("");
+                $("#modal-term").modal("show");
+            },
+            success: function (data) {
+                $("#modal-term .modal-content").html(data.html_form);
+            }
+        });
+    };
 
-  var saveForm = function () {
-    var form = $(this);
-    $.ajax({
-      url: form.attr("action"),
-      data: form.serialize(),
-      type: form.attr("method"),
-      dataType: 'json',
-      success: function (data) {
-        if (data.form_is_valid) {
-          $(".defi-software-app").html(data.html_term_list);
-          $("#modal-term").modal("hide");
-        }
-        else {
-          $("#modal-term .modal-content").html(data.html_form);
-        }
-      }
-    });
-    return false;
-  };
+    var saveForm = function () {
+        var form = $(this);
+        $.ajax({
+            url: form.attr("action"),
+            data: form.serialize(),
+            type: form.attr("method"),
+            dataType: 'json',
+            success: function (data) {
+                if (data.form_is_valid) {
+                    $(".defi-software-app").html(data.html_term_list);
+                    $("#modal-term").modal("hide");
+                } else {
+                    $("#modal-term .modal-content").html(data.html_form);
+                }
+            }
+        });
+        return false;
+    };
 
 
-  /* Binding */
+    /* Binding */
 
-  // Create term
-  $(".js-create-term").click(loadForm);
-  $('#modal-term').on("submit", ".js-term-create-form", saveForm);
+    // Create term
+    $(".js-create-term").click(loadForm);
+    $('#modal-term').on("submit", ".js-term-create-form", saveForm);
 
-  // Update term
-  $(".defi-software-app").on("click", ".js-update-term", loadForm);
-  $("#modal-term").on("submit", ".js-term-update-form", saveForm);
+    // Update term
+    $(".defi-software-app").on("click", ".js-update-term", loadForm);
+    $("#modal-term").on("submit", ".js-term-update-form", saveForm);
 
-  // Delete term
-  $(".defi-software-app").on("click", ".js-delete-term", loadForm);
-  $("#modal-term").on("submit", ".js-term-delete-form", saveForm);
+    // Delete term
+    $(".defi-software-app").on("click", ".js-delete-term", loadForm);
+    $("#modal-term").on("submit", ".js-term-delete-form", saveForm);
 
 });
