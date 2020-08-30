73249626af675ad8b7d37f0134faec07f1cd66ff
evmcodi <emaccuarta@protonmail.com>
2020-08-30 06:31:35 +0100

    fix: Return configuration to original setting. DISABLE_COLLECTSTATIC doesn't act as a boolean in Heroku, and must be removed entirely from the Heroku config instead.

--- definitions/settings.py
+++ definitions/settings.py
@@ -175,8 +175,8 @@ if 'USE_AWS' in os.environ:
 
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'definitions-web-app'
-    # AWS_S3_REGION_NAME = 'eu-west-1'
-    AWS_DEFAULT_REGION = 'eu-west-1'
+    AWS_S3_REGION_NAME = 'eu-west-1'
+    # AWS_DEFAULT_REGION = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
